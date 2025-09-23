d4291be90790d3f2c750b7c3fa0511837873829a
Rushab Rayalwar <rushabrayalwar@gmail.com>
2025-09-23 23:56:53 +0530

    Add the FriendshipCard component

--- Actual App/sociallyzer/src/pages/FriendsPage/FriendsPage.jsx
+++ Actual App/sociallyzer/src/pages/FriendsPage/FriendsPage.jsx
@@ -7,13 +7,14 @@ import styles from "./FriendsPage.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
 import FriendRequest from "../../components/FriendRequest/FriendRequest";
+import FriendshipCard from "../../components/FriendshipCard/FriendshipCard";
 
 // images / svgs
 import icon from "../../assets/icons/icon.svg";
 import postIMG1 from "../../assets/dummyPosts/Screenshot 2024-02-15 013817.jpg";
 
 export default function FriendsPage(){
-    const [status, setStatus] = useState("left");
+    const [tab, setTab] = useState("left");
     return(
         <>
             <div className={styles.friendsPageContainer}>
@@ -27,14 +28,14 @@ export default function FriendsPage(){
                     transition={{type:"spring", stiffness:120, damping:10, mass:0.9}}
                     >
                         <div className={styles.bodyHeader}>
-                            <div className={`${styles.bodyTabName} ${styles.selected}`}>
+                            <div className={`${styles.bodyTabName} ${styles.selected}`} onClick={()=>setTab('left')}>
                                 Manage Requests
                             </div>
-                            <div className={styles.bodyTabName}>
+                            <div className={styles.bodyTabName} onClick={()=>setTab('right')}>
                                 Manage Friendships
                             </div>
                         </div>
-                        <div className={styles.bodyMain}>
+                        {tab=="left" && <div className={`${styles.bodyMain} ${styles.requests}`}>
                             <FriendRequest name="John Doe" friendsInCommon="5"/>
                             
                             <FriendRequest name="John Doe" friendsInCommon="5"/>
@@ -67,7 +68,41 @@ export default function FriendsPage(){
                             
                             <FriendRequest name="John Doe" friendsInCommon="5"/>
                             
-                        </div>
+                        </div>}
+                        {tab=="right" && <div className={`${styles.bodyMain} ${styles.friendships}`}>
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5"/>
+                            
+                        </div>}
                     </motion.section>
                 </div>
             </div>
