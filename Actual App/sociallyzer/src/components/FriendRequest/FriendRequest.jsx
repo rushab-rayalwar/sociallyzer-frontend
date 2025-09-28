c353686cee71f469939f20c4bf2d2981e2d76e8b
Rushab Rayalwar <rushabrayalwar@gmail.com>
2025-09-28 23:28:42 +0530

    Implement staggered animation for FriendRequest list

--- Actual App/sociallyzer/src/components/FriendRequest/FriendRequest.jsx
+++ Actual App/sociallyzer/src/components/FriendRequest/FriendRequest.jsx
@@ -1,9 +1,26 @@
+//external imports
+import { motion } from "framer-motion";
+
+// local imports
 import styles from "./FriendRequest.module.css";
 
-export default function FriendRequest({name, friendsInCommon}){
+export default function FriendRequest({name, friendsInCommon, enableBlur, handleHovering, handleHoveringStop}){
+    const variants = {
+        hidden:{
+            opacity:0,
+            y: "+30%"
+        },
+        visible:{
+            opacity:1,
+            y: "0"
+        }
+    }
     return(
         <>
-            <div className={styles.friendRequest}>
+            <motion.div className={enableBlur ? `${styles.blurCard} ${styles.friendRequest}` : `${styles.noBlurCard} ${styles.friendRequest}`} onMouseEnter={handleHovering} onMouseLeave={handleHoveringStop}
+            variants={variants}
+            transition={{duration:0.65}}
+            >
                 <div className={styles.left}>
                     <div className={styles.profilePic}>
                         <img src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="profile pic"/>
@@ -17,7 +34,7 @@ export default function FriendRequest({name, friendsInCommon}){
                     <div className={`${styles.acceptButton} ${styles.button}`}>Accept</div>
                     <div className={`${styles.deleteButton} ${styles.button}`}>Delete</div>
                 </div>
-            </div>
+            </motion.div>
         </>
     )
 }
--- Actual App/sociallyzer/src/pages/FriendsPage/FriendsPage.jsx
+++ Actual App/sociallyzer/src/pages/FriendsPage/FriendsPage.jsx
@@ -16,6 +16,16 @@ import postIMG1 from "../../assets/dummyPosts/Screenshot 2024-02-15 013817.jpg";
 export default function FriendsPage(){
     const [tab, setTab] = useState("left");
     const [hoveringOver, setHoveringOver] = useState(null);
+
+    const containerVariants = {
+        hidden : {
+            opacity : 0
+        },
+        visible : {
+            opacity : 1,
+            transition:{staggerChildren:0.05, delayChildren:0.1, duration:0.1, ease:"easeOut"}
+        }
+    }
     return(
         <>
             <div className={styles.friendsPageContainer}>
@@ -26,7 +36,7 @@ export default function FriendsPage(){
                     <motion.section className={styles.body}
                     initial={{opacity:0, y:50}}
                     animate={{opacity:1, y:0}}
-                    transition={{type:"spring", stiffness:120, damping:10, mass:0.9}}
+                    transition={{type:"spring", stiffness:120, damping:15, mass:0.9}}
                     >
                         <div className={styles.bodyHeader}>
                             <div className={tab=="left" ? `${styles.bodyTabName} ${styles.selected}` : styles.bodyTabName} onClick={()=>setTab('left')}>
@@ -36,44 +46,48 @@ export default function FriendsPage(){
                                 Manage Friendships
                             </div>
                         </div>
-                        {tab=="left" && <div className={`${styles.bodyMain} ${styles.requests}`}>
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='1'  enableBlur={( hoveringOver=='1' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('1')} handleHoveringStop={()=>setHoveringOver(null)} />
+                        {tab=="left" && <motion.div className={`${styles.bodyMain} ${styles.requests}`}
+                        variants={containerVariants}
+                        initial='hidden'
+                        animate='visible'
+                        >
+                            <FriendRequest name="Rohan Rayalwar" friendsInCommon="5" key='1'  enableBlur={( hoveringOver=='1' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('1')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='2' enableBlur={( hoveringOver=='2' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('2')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Sheetal Rayalwar" friendsInCommon="5" key='2' enableBlur={( hoveringOver=='2' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('2')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
                             <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='3' enableBlur={( hoveringOver=='3' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('3')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='4' enableBlur={( hoveringOver=='4' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('4')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Shourya Rayalwar" friendsInCommon="5" key='4' enableBlur={( hoveringOver=='4' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('4')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='5' enableBlur={( hoveringOver=='5' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('5')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Gourang Rayalwar" friendsInCommon="5" key='5' enableBlur={( hoveringOver=='5' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('5')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='6' enableBlur={( hoveringOver=='6' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('6')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Aditya Walture" friendsInCommon="5" key='6' enableBlur={( hoveringOver=='6' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('6')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='7' enableBlur={( hoveringOver=='7' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('7')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Pratik Kurhe" friendsInCommon="5" key='7' enableBlur={( hoveringOver=='7' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('7')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='8' enableBlur={( hoveringOver=='8' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('8')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Yuvraj Rathore" friendsInCommon="5" key='8' enableBlur={( hoveringOver=='8' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('8')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendRequest name="Tatya Vinchu" friendsInCommon="5" key='9' enableBlur={( hoveringOver=='9' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('9')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendRequest name="Ravi Rathore" friendsInCommon="5" key='9' enableBlur={( hoveringOver=='9' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('9')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                        </div>}
+                        </motion.div>}
                         {tab=="right" && <div className={`${styles.bodyMain} ${styles.friendships}`}>
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='1'  enableBlur={( hoveringOver=='1' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('1')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Gourang Rayalwar" friendsInCommon="5" key='1'  enableBlur={( hoveringOver=='1' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('1')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='2' enableBlur={( hoveringOver=='2' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('2')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Ravi Rathore" friendsInCommon="5" key='2' enableBlur={( hoveringOver=='2' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('2')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
                             <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='3' enableBlur={( hoveringOver=='3' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('3')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='4' enableBlur={( hoveringOver=='4' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('4')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Pratik Kurhe" friendsInCommon="5" key='4' enableBlur={( hoveringOver=='4' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('4')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='5' enableBlur={( hoveringOver=='5' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('5')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Shourya Rayalwar" friendsInCommon="5" key='5' enableBlur={( hoveringOver=='5' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('5')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='6' enableBlur={( hoveringOver=='6' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('6')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Sheetal Rayalwar" friendsInCommon="5" key='6' enableBlur={( hoveringOver=='6' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('6')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='7' enableBlur={( hoveringOver=='7' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('7')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Rohan Rayalwar" friendsInCommon="5" key='7' enableBlur={( hoveringOver=='7' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('7')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='8' enableBlur={( hoveringOver=='8' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('8')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Gotu Dada" friendsInCommon="5" key='8' enableBlur={( hoveringOver=='8' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('8')} handleHoveringStop={()=>setHoveringOver(null)} />
                             
-                            <FriendshipCard name="Tatya Vinchu" friendsInCommon="5" key='9' enableBlur={( hoveringOver=='9' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('9')} handleHoveringStop={()=>setHoveringOver(null)} />
+                            <FriendshipCard name="Yogu Dada" friendsInCommon="5" key='9' enableBlur={( hoveringOver=='9' || hoveringOver==null ) ? false : true } handleHovering={()=>setHoveringOver('9')} handleHoveringStop={()=>setHoveringOver(null)} />
                         </div>}
                     </motion.section>
                 </div>
