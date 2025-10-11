3b38daa75ed896135023ef0a415f72ccf9b7642a
Rushab Rayalwar <rushabrayalwar@gmail.com>
2025-10-11 23:52:06 +0530

    Improve staggered animation for SavedPost component

--- Actual App/sociallyzer/src/pages/SavedPostsPage/SavedPostsPage.jsx
+++ Actual App/sociallyzer/src/pages/SavedPostsPage/SavedPostsPage.jsx
@@ -1,5 +1,6 @@
 // third-party imports
 import {motion} from "framer-motion";
+import { useState } from "react";
 
 // local imports
 import styles from "./SavedPostsPage.module.css";
@@ -8,6 +9,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import SavedPost from "../../components/SavedPost/SavedPost";
 
 export default function SavedPostsPage(){
+    const [hoveringOver, setHoveringOver] = useState(null);
     return(
         <>
             <div className={styles.savedPostsPageContainer}>
@@ -23,23 +25,23 @@ export default function SavedPostsPage(){
                             initial="hidden"
                             animate="show"
                             >
-                                <SavedPost imageNumber="1"/>
-                                <SavedPost imageNumber="3"/>
-                                <SavedPost imageNumber="2"/>
-                                <SavedPost imageNumber="1"/>
-                                <SavedPost imageNumber="3"/>
-                                <SavedPost imageNumber="2"/>
-                                <SavedPost imageNumber="3"/>
-                                <SavedPost imageNumber="1"/>
-                                <SavedPost imageNumber="2"/>
-                                <SavedPost imageNumber="3"/>
-                                <SavedPost imageNumber="2"/>
-                                <SavedPost imageNumber="1"/>
-                                <SavedPost imageNumber="2"/>
-                                <SavedPost imageNumber="1"/>
-                                <SavedPost imageNumber="3"/>
-                                <SavedPost imageNumber="1"/>
-                                <SavedPost imageNumber="2"/>
+                                <SavedPost imageNumber="1" key="17" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(17)}/>
+                                <SavedPost imageNumber="3" key="16" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(16)}/>
+                                <SavedPost imageNumber="2" key="15" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(15)}/>
+                                <SavedPost imageNumber="1" key="14" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(14)}/>
+                                <SavedPost imageNumber="3" key="13" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(13)}/>
+                                <SavedPost imageNumber="2" key="12" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(12)}/>
+                                <SavedPost imageNumber="3" key="11" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(11)}/>
+                                <SavedPost imageNumber="1" key="10" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(10)}/>
+                                <SavedPost imageNumber="2" key="9" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(9)}/>
+                                <SavedPost imageNumber="3" key="8" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(8)}/>
+                                <SavedPost imageNumber="2" key="7" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(7)}/>
+                                <SavedPost imageNumber="1" key="6" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(6)}/>
+                                <SavedPost imageNumber="2" key="5" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(5)}/>
+                                <SavedPost imageNumber="1" key="4" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(4)}/>
+                                <SavedPost imageNumber="3" key="3" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(3)}/>
+                                <SavedPost imageNumber="1" key="2" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(2)}/>
+                                <SavedPost imageNumber="2" key="1" onMouseLeave={()=>setHoveringOver(null)} onMouseEnter={()=>setHoveringOver(1)}/>
                             </motion.div>
                         </div>
                     </section>
