71b70310beb12b36d552eca80a63c76b206d0cbe
Rushab Rayalwar <rushabrayalwar@gmail.com>
2026-02-08 21:36:22 +0530

    implement animations for toast notification

--- Actual App/sociallyzer/src/components/ToastNotification/ToastNotification.jsx
+++ Actual App/sociallyzer/src/components/ToastNotification/ToastNotification.jsx
@@ -1,19 +1,29 @@
 // third party imports
-import {motion} from "framer-motion";
+import {motion, AnimatePresence} from "framer-motion";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 // local imports
 import styles from "./ToastNotification.module.css";
 
 export default function ToastNotification({message}){
+    const [isClosed, setIsClosed] = useState(false);
     return (
         <>
-            <motion.div className={styles.container}
-            initial={{opacity:0, filter:"blur(0.4rem)", y:"1rem"}}
-            animate={{opacity:1, filter:"blur(0)", y:0}}
-            transition={{duration:0.5, }}
-            >
-                <div className={styles.content}>{message}</div>
-            </motion.div>
+            <AnimatePresence mode="wait">
+                {!isClosed && 
+                    <motion.div className={styles.container} onClick={()=>setIsClosed(true)}
+                    initial={{opacity:0, filter:"blur(0.4rem)", y:"1rem"}}
+                    animate={{opacity:1, filter:"blur(0)", y:0}}
+                    exit={{opacity:0, filter:"blur(0.4rem)", y:"1rem"}}
+                    transition={{duration:0.5, }}
+                    >
+                        <div className={styles.content}>{message}</div>
+                        <FontAwesomeIcon className={styles.closeIcon} icon={faXmark}/>
+                    </motion.div>
+                }
+            </AnimatePresence>
         </>
     )
 }
